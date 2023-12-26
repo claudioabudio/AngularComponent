a0aee3f0d448f6cfa96a3021654cbda61481bfab
Claudio Abud <ca9234@us.att.com>
2023-12-25 23:06:08 -0600

    added new notification component that uses the template property instead of templateUrl

--- src/app/Container/container.component.ts
+++ src/app/Container/container.component.ts
@@ -1,13 +1,14 @@
 import { Component } from "@angular/core";
 import { NavComponent } from "../nav/nav.component";
 import { HeaderComponent } from "../header/header.component";
+import { NotificationComponent } from "../notification/notification.component";
 
 @Component({
     selector: 'app-container',
     standalone: true,
     templateUrl: './container.component.html',
     styleUrls: ['./container.component.css'],
-    imports: [NavComponent, HeaderComponent]
+    imports: [NavComponent, HeaderComponent, NotificationComponent]
 })
 export class ContainerComponent {
 
---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  standalone: true,
+  imports: [],
+  template: '<div><p>This website uses cookies to provide a better user experience</p></div>',
+  styleUrl: './notification.component.css'
+})
+export class NotificationComponent {
+
+}
