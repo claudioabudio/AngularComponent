4a4c8a567afc5ffef783014e50274e029a098f16
Claudio Abud <ca9234@us.att.com>
2023-12-26 22:06:45 -0600

    added styles property to the NotificationComponent

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -5,7 +5,9 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: '<div><p>This website uses cookies to provide a better user experience</p></div>',
-  styleUrl: './notification.component.css'
+  styles: [`div {margin: 10px 0px; padding: 10px 20px; background-color: #fad7a0; text-align: center}`, 
+  `p {font-size: 16px}`
+]
 })
 export class NotificationComponent {
 
