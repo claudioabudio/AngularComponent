c2f619785932dfa99ffecbd8f34103a98ac6a2f3
Claudio Abud <ca9234@us.att.com>
2023-12-28 02:22:06 -0600

    added searh input elem and close button for the notification

--- src/app/Container/container.component.ts
+++ src/app/Container/container.component.ts
@@ -2,13 +2,14 @@ import { Component } from "@angular/core";
 import { NavComponent } from "../nav/nav.component";
 import { HeaderComponent } from "../header/header.component";
 import { NotificationComponent } from "../notification/notification.component";
+import { SearchComponent } from "../search/search.component";
 
 @Component({
     selector: 'app-container',
     standalone: true,
     templateUrl: './container.component.html',
     styleUrls: ['./container.component.css'],
-    imports: [NavComponent, HeaderComponent, NotificationComponent]
+    imports: [NavComponent, HeaderComponent, NotificationComponent, SearchComponent]
 })
 export class ContainerComponent {
 
--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -5,11 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `<div class="alert alert-success " [hidden]="!displayNotification">
-    <p>This website uses cookies to provide a better user experience</p></div>`,
+    <p>This website uses cookies to provide a better user experience</p>
+    <div class="close"><button class="btn" (click)="closeNotification($event)">X</button></div>
+    </div>`,
   styles: [`.notification-div {margin: 10px 0px; padding: 10px 20px; background-color: #fad7a0; text-align: center}`, 
-  `p {font-size: 16px}`
+  `p {font-size: 16px}`,
+  `.close {float: right; margin-top: -45px}`
 ]
 })
 export class NotificationComponent {
+
+closeNotification(event: Event) {
+  this.displayNotification = false;
+}
   displayNotification: boolean = true
 }
