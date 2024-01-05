b482c3b7389f561062053d811445cccefcce19a6
Claudio Abud <ca9234@us.att.com>
2024-01-04 22:40:19 -0600

    uses two way data binding [(ngModel)] directive

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-    searchValue : string = ""
+    searchValue : string = "iPhone"
 
     changeSearchValue(eventData: Event) {
       // console.log((<HTMLInputElement>eventData.target).value);
